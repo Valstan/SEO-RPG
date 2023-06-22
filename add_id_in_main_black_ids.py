12608b1d0b06666f6f64bb62770b809805beb8a6
Валентин <123qwemmm@mail.ru>
2023-06-22 21:28:26 +0300

    упрощение добавления группы в черный список

--- add_id_in_main_black_ids.py
+++ add_id_in_main_black_ids.py
@@ -9,12 +9,24 @@ except:
 
 while True:
 
-    black_id = (abs(int(input("Введите ID группы для занесения в черный список: "))))
-    if not black_id:
-        break
+    str_black_id = input("Введите ID группы для занесения в черный список: ")
+
+    if not str_black_id:
+        quit()
+
+    if 'wall' in str_black_id:
+        text_list = str_black_id.split(sep="wall")
+        text_list = text_list[1].split(sep="_")
+        black_id = abs(int(text_list[0]))
+    else:
+        black_id = abs(int(str_black_id))
+
+    print(black_id)
+
     if black_id in main_black_ids:
         print("Данный ID уже есть в базе данных запрещенных ID")
     else:
         main_black_ids.append(black_id)
         with open(os.path.join("base/main_black_ids.json"), 'w', encoding='utf-8') as f:
             f.write(json.dumps(main_black_ids, indent=2, ensure_ascii=False))
+        print("Группа добавлена в баню.")
